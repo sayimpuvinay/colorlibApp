bfbf5831110d2c03930dbaafd4535b8ca7205b56
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-07-30 18:10:22 +0530

    layout changed for login page

--- src/app/app-pages/app-pages.module.ts
+++ src/app/app-pages/app-pages.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { CarrersComponent } from './carrers/carrers.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TechnologyComponent,
+    CarrersComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/app-pages/carrers/carrers.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarrersComponent } from './carrers.component';
+
+describe('CarrersComponent', () => {
+  let component: CarrersComponent;
+  let fixture: ComponentFixture<CarrersComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CarrersComponent]
+    });
+    fixture = TestBed.createComponent(CarrersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
